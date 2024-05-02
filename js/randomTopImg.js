1a2d8c4364561c231bf4430e6557568e1a04ecbb
yee1819 <103957720+yee1819@users.noreply.github.com>
2024-05-02 20:46:48 +0800

    Site updated: 2024-05-02 20:46:48

--- js/randomTopImg.js
+++ js/randomTopImg.js
@@ -1,8 +1,12 @@
 
-fetch('../json/output.json')
+const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+const jsonUrl = isMobile ? '../json/m_top_img.json' : '../json/top_img.json';
+
+fetch(jsonUrl)
   .then(response => {
     if (!response.ok) {
-      throw new Error('Network response was not ok');
+      throw new Error('网络响应异常');
     }
     return response.json();
   })
@@ -17,9 +21,47 @@ fetch('../json/output.json')
     console.log(imgDivTopUrl);
   })
   .catch(error => {
-    console.error('There was a problem with your fetch operation:', error);
+    console.error('获取数据时发生问题:', error);
   });
 
-  function getRandomInt(min, max) {
+function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
+
+
+
+///以下是根据屏幕变化修改导航栏的 宽度
+
+
+var screenWidth;
+var screenHeight ;
+  function updateScreenSize() {
+    screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    screenHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+    // document.getElementById('screenSizeOutput').textContent = `Screen Width: ${screenWidth}px, Screen Height: ${screenHeight}px`;
+  }
+
+  // Initial call to update screen size information
+  updateScreenSize();
+
+  // Event listener for window resize event
+  window.addEventListener('resize', updateScreenSize);
+
+  function updateScrollDistance() {
+    const scrollDistance = window.pageYOffset || document.documentElement.scrollTop;
+    const kirari_box_nav = document.querySelectorAll('.kirari-nav-box');
+
+  kirari_box_nav.forEach(function(element) {
+    if (scrollDistance < screenHeight*7/10) {
+      element.style.maxWidth = `${screenWidth}px`;
+    } else {
+      element.style.maxWidth = '1170px';
+    }
+  });
+  }
+
+  // Initial call to update scroll distance
+  updateScrollDistance();
+
+  // Event listener for scroll event
+  window.addEventListener('scroll', updateScrollDistance);
