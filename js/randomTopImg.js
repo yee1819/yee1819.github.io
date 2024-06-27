58a949ac1e648835ab1a5f522f6a777953140dea
yee1819 <103957720+yee1819@users.noreply.github.com>
2024-06-27 18:04:43 +0800

    Site updated: 2024-06-27 18:04:42

--- js/randomTopImg.js
+++ js/randomTopImg.js
@@ -2,22 +2,22 @@
 const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
 const jsonUrl = isMobile ? '../json/m_top_img.json' : '../json/top_img.json';
-const bg_json = "../json/bg.json"
+// const bg_json = "../json/bg.json"
 // 随机背景图
-fetch(bg_json)
-  .then(response => response.json())
-  .then(data => {
-    const bg_json_len = data.color.length;
-    const random_bg = getRandomInt(0, bg_json_len);
-    const bg_color = document.getElementById("body-wrap");
-    const bg_ran_re = data.color[random_bg];
-    bg_color.style.background = bg_ran_re.c1;
-    bg_color.style.background = bg_ran_re.c2;
-    bg_color.style.background = bg_ran_re.c3;
-  })
-  .catch(error => {
-    console.error('Error fetching data:', error);
-  });
+// fetch(bg_json)
+//   .then(response => response.json())
+//   .then(data => {
+//     const bg_json_len = data.color.length;
+//     const random_bg = getRandomInt(0, bg_json_len);
+//     const bg_color = document.getElementById("body-wrap");
+//     const bg_ran_re = data.color[random_bg];
+//     bg_color.style.background = bg_ran_re.c1;
+//     bg_color.style.background = bg_ran_re.c2;
+//     bg_color.style.background = bg_ran_re.c3;
+//   })
+//   .catch(error => {
+//     console.error('Error fetching data:', error);
+//   });
 
 // 头顶图随机
 fetch(jsonUrl)
@@ -30,10 +30,10 @@ fetch(jsonUrl)
   .then(data => {
 
     var img_length_top = data.links.length;
-    var random_img_top =  getRandomInt(0,img_length_top);
+    var random_img_top = getRandomInt(0, img_length_top);
     var img_top = document.getElementById("page-header");
-    var imgDivTopUrl = data.links[random_img_top]; 
-    
+    var imgDivTopUrl = data.links[random_img_top];
+
     img_top.style.backgroundImage = 'url(' + imgDivTopUrl + ')';
 
     console.log(imgDivTopUrl);
@@ -52,48 +52,66 @@ function getRandomInt(min, max) {
 
 
 var screenWidth;
-var screenHeight ;
-  function updateScreenSize() {
-    screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-    screenHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-    // document.getElementById('screenSizeOutput').textContent = `Screen Width: ${screenWidth}px, Screen Height: ${screenHeight}px`;
-  }
+var screenHeight;
+function updateScreenSize() {
+  screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  screenHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+  // document.getElementById('screenSizeOutput').textContent = `Screen Width: ${screenWidth}px, Screen Height: ${screenHeight}px`;
+}
 
-  // Initial call to update screen size information
-  updateScreenSize();
+// Initial call to update screen size information
+updateScreenSize();
 
-  // Event listener for window resize event
-  window.addEventListener('resize', updateScreenSize);
+// Event listener for window resize event
+window.addEventListener('resize', updateScreenSize);
 
-  function updateScrollDistance() {
-    const scrollDistance = window.pageYOffset || document.documentElement.scrollTop;
-    const kirari_box_nav = document.querySelectorAll('.kirari-nav-box');
+function updateScrollDistance() {
+  const scrollDistance = window.pageYOffset || document.documentElement.scrollTop;
+  const kirari_box_nav = document.querySelectorAll('.kirari-nav-box');
 
-  kirari_box_nav.forEach(function(element) {
-    if (scrollDistance < screenHeight*7/10) {
+  kirari_box_nav.forEach(function (element) {
+    if (scrollDistance < screenHeight * 7 / 10) {
       element.style.maxWidth = `${screenWidth}px`;
     } else {
       element.style.maxWidth = '1170px';
     }
   });
-  }
+}
 
-  // Initial call to update scroll distance
-  updateScrollDistance();
+// Initial call to update scroll distance
+updateScrollDistance();
 
-  // Event listener for scroll event
-  window.addEventListener('scroll', updateScrollDistance);
+// Event listener for scroll event
+window.addEventListener('scroll', updateScrollDistance);
 
 
 
 //sakana
-  document.addEventListener('DOMContentLoaded', function() {
-    // let i=0;
-    var sakanawidgetctrl = document.querySelectorAll('.sakana-widget-ctrl a');
-    sakanawidgetctrl.forEach(function(element) {
-        // i+=1;
-        // if(i==3){
-          element.style.display = 'none';
-        // }
-    });
+document.addEventListener('DOMContentLoaded', function () {
+  // let i=0;
+  var sakanawidgetctrl = document.querySelectorAll('.sakana-widget-ctrl a');
+  sakanawidgetctrl.forEach(function (element) {
+    // i+=1;
+    // if(i==3){
+    element.style.display = 'none';
+    // }
+  });
 });
+
+/**
+ * 版权网址换为当前网址
+ */
+window.onload = function () {
+  // 获取当前网址
+  let currentURL = window.location.href;
+
+  // 获取所有具有 post-copyright-info 类的 <span> 标签
+  let links = document.querySelectorAll('.post-copyright__type>.post-copyright-info a');
+
+  // 遍历这些 <span> 标签并更改其内部 <a> 标签的 href 属性和内容
+  links.forEach(function (link) {
+    link.href = currentURL;
+    link.textContent = currentURL;
+  });
+};
+
